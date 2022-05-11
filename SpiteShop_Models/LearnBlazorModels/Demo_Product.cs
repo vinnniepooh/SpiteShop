33897925bc606fcd65c502e2a5e98427632b1c46
Vinh Nguyen <vnguyen@toptechnologies.de>
2022-05-11 14:39:18 +0200

    First

---
+++ SpiteShop_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiteShop_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool isActive { get; set; }
+
+        public List<Demo_ProductProp> ProductProperties { get; set; }
+
+}
+}
---
+++ SpiteShop_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiteShop_Models.LearnBlazorModels
+{
+    public class Demo_ProductProp
+    {
+        public int ID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}

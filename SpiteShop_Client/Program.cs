694e5d794c634c8348de22e4671636bb2cd49035
Vinh Nguyen <vnguyen@toptechnologies.de>
2022-06-10 16:33:01 +0200

    Revert "ver 2"
    This reverts commit 1d0791a8218c1c089f273b07b4fbd25d9b7e901b.

--- SpiteShop_Client/Program.cs
+++ SpiteShop_Client/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpiteShop_Client;
-using SpiteShop_Client.Service;
-using SpiteShop_Client.Service.IService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<string>("BaseAPIUrl"))});
-builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 await builder.Build().RunAsync();
